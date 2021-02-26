125ed5254e6f94e897bf153c2df80a522f6f0d2f
Eduardo <datosmoviles52@gmail.com>
2021-02-26 14:37:48 -0600

    initial commit

---
+++ src/corto2/Corto2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package corto2;
+
+/**
+ *
+ * @author eduab
+ */
+public class Corto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola de nuevo Eduardo :) tanto tiempo, como has crecido");
+    }
+    
+}
